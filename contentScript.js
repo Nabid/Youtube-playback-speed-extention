8dba343dcbae68adbcbcb09f83ead55a39af8d36
nabid.imteaj <nabid.imteaj@e-consult.eco>
2019-11-15 16:45:03 +0100

    updated t and c

--- contentScript.js
+++ contentScript.js
@@ -1,25 +1,20 @@
 function triggerPlaybackSpeed150() {
-    //$('.ytp-settings-menu .ytp-panel .ytp-panel-menu .ytp-menuitem:nth-child(6)').click();
     document.querySelector(".ytp-settings-menu .ytp-panel .ytp-panel-menu .ytp-menuitem:nth-child(6)").click();
 }
 
 function triggerPlaybackSpeed125() {
-    //$('.ytp-settings-menu .ytp-panel .ytp-panel-menu .ytp-menuitem:nth-child(5)').click();
     document.querySelector(".ytp-settings-menu .ytp-panel .ytp-panel-menu .ytp-menuitem:nth-child(5)").click();
 }
 
 function triggerPlaybackSpeedNormal() {
-    //$('.ytp-settings-menu .ytp-panel .ytp-panel-menu .ytp-menuitem:nth-child(4)').click();
     document.querySelector(".ytp-settings-menu .ytp-panel .ytp-panel-menu .ytp-menuitem:nth-child(4)").click();
 }
 
 function triggerPlaybackSpeedDialog() {
-    //$('.ytp-settings-menu .ytp-panel .ytp-panel-menu .ytp-menuitem:nth-child(3)').click()
     document.querySelector(".ytp-settings-menu .ytp-panel .ytp-panel-menu .ytp-menuitem:nth-child(3)").click();
 }
 
 function toggleSettingsButton() {
-    //$('.ytp-settings-button').click();
     document.querySelector(".ytp-settings-button").click();
 }
 
@@ -61,23 +56,22 @@ function triggerPlayback(triggerValue) {
 function init() {
     document.onkeydown = function (e) {
         if( e.which === 49 && e.altKey ) {
-            console.log('you pressed alt + 1');
+            //console.log('you pressed alt + 1');
             triggerPlayback(1);
         }
 
         if( e.which === 50 && e.altKey ) {
-            console.log('you pressed alt + 2');
+            //console.log('you pressed alt + 2');
             triggerPlayback(2);
         }
 
         if( e.which === 51 && e.altKey ) {
-            console.log('you pressed alt + 3');
+            //console.log('you pressed alt + 3');
             triggerPlayback(3);
         }
     };
 }
 
 setTimeout(function() {
-    //debugger
     init();
 }, 2000);
